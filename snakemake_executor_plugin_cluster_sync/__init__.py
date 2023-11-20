7f195233843c13b938a4a01deb482a0a9320420f
Johannes Koester <johannes.koester@uni-due.de>
2023-11-20 12:25:12 +0100

    fix import

--- snakemake_executor_plugin_cluster_sync/__init__.py
+++ snakemake_executor_plugin_cluster_sync/__init__.py
@@ -7,7 +7,7 @@ from typing import List, Generator, Optional
 from snakemake_interface_common.exceptions import WorkflowError
 from snakemake_interface_executor_plugins.executors.base import SubmittedJobInfo
 from snakemake_interface_executor_plugins.executors.remote import RemoteExecutor
-from snakemake_interface_executor_plugins import ExecutorSettingsBase, CommonSettings
+from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase, CommonSettings
 from snakemake_interface_executor_plugins.jobs import (
     JobExecutorInterface,
 )
