4c7e38b461a8560f7e1a18da84c4e3a19597b95b
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-10-17 13:53:48 +0200

    misc

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ from typing import Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 
-from snakemake_executor_plugin_cluster_sync import ExecutorSettings
+from snakemake_executor_plugin_mpcdf import ExecutorSettings
 
 
 class TestWorkflows(snakemake.common.tests.TestWorkflowsLocalStorageBase):
@@ -13,7 +13,7 @@ class TestWorkflows(snakemake.common.tests.TestWorkflowsLocalStorageBase):
         return str((Path(__file__).parent / cmd).absolute())
 
     def get_executor(self) -> str:
-        return "cluster-sync"
+        return "mpcdf"
 
     def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
-        return ExecutorSettings(submit_cmd=self._get_cmd("qsub.sh"))
+        return ExecutorSettings()
